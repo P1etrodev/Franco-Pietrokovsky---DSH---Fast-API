53b043ae5c17c1b84b449c1a32ecfc5f87b405fd
Sylph1de <IlluminatiGrizzly@gmail.com>
2022-09-19 15:09:48 -0300

    bah

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 import os
 
-USER = 'root'#os.environ['USER']
-PASSWORD = 'eEITBL9hIeOrKDCmtifM'#os.environ['PASSWORD']
-HOST = 'containers-us-west-61.railway.app'#os.environ['HOST']
-DB = 'railway'#os.environ['DB']
+USER = "root"  # os.environ['USER']
+PASSWORD = "eEITBL9hIeOrKDCmtifM"  # os.environ['PASSWORD']
+HOST = "containers-us-west-61.railway.app"  # os.environ['HOST']
+DB = "railway"  # os.environ['DB']
 
-DB_URL = "mysql+pymysql://{}:{}@{}:3306/{}".format(USER, PASSWORD, HOST, DB)
+DB_URL = "mysql+pymysql://{}:{}@{}:6913/{}".format(USER, PASSWORD, HOST, DB)
 
 engine = create_engine(DB_URL, pool_pre_ping=True)
 
